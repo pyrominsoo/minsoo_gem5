74f1d427ae4a5dc5f93307ff1898419e537ee9b0
Min Soo Kim <minsk1@uci.edu>
2017-10-04 15:50:02 -0700

    Branch testing Please ignore
    Change-Id: Ieac60d11f080fb82185fe99eac4a3897904585a8

--- configs/tutorial/simple.py
+++ configs/tutorial/simple.py
@@ -26,6 +26,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Authors: Jason Power
+# MINSOO TEST
 
 """ This file creates a barebones system and executes 'hello', a simple Hello
 World application.
