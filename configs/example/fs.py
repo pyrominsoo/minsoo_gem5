bdc30f5dd5e56ef2e3128441abbd1ddbe2edc285
Min Soo Kim <minsk1@uci.edu>
2017-10-19 16:10:10 -0700

    Reverted fs.py as it caused the connection problem on normal full system mode Removed the disabiling of the listening
    Change-Id: I5e5ab0d88f7e2c6eb39f5827bb3b40d29b0bbc07

--- configs/example/fs.py
+++ configs/example/fs.py
@@ -83,6 +83,10 @@ def cmd_line_template():
     return None
 
 def build_test_system(np):
+<<<<<<< Updated upstream
+=======
+    #m5.disableAllListeners()
+>>>>>>> Stashed changes
     cmdline = cmd_line_template()
     if buildEnv['TARGET_ISA'] == "alpha":
         test_sys = makeLinuxAlphaSystem(test_mem_mode, bm[0], options.ruby,
